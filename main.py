29995018653ea4d40c258a0a63dcee2a23ed3f76
mojtaba <mojtababht@gmail.com>
2024-07-21 16:40:09 +0330

    -

--- main.py
+++ main.py
@@ -1,50 +1,88 @@
-from tkinter import *
-from tkinter import filedialog
-from PIL import Image
-from numpy import asarray
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 
+class CNN(nn.Module):
 
-
-class Screen(Tk):
     def __init__(self):
         super().__init__()
-        self.geometry('300x300')
-        self.label_file_explorer = Label(self,
-                                    text="File Explorer using Tkinter",
-                                    width=100, height=4,
-                                    fg="blue")
-        self.label_file_explorer.pack()
-        self.file_input = Button(self, text='file', command=self.browse_files)
-        self.file_input.pack()
-        self.button = Button(self, text='submit', command=self.submit_image)
-        self.button.pack()
-        self.filename = None
-        self.file = None
 
-    def browse_files(self):
-        self.filename = filedialog.askopenfilename(initialdir = "",
-                                              title = "Select a File",
-                                              filetypes = (("Text files",
-                                                            "*.png*"),
-                                                           ("all files",
-                                                            "*.*")))
-        self.label_file_explorer.configure(text=self.filename)
-        with open(self.filename) as file:
-            self.file = file
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-    def submit_image(self):
-        if self.filename:
-            image = Image.open(self.filename)
-            image = image.convert('L')
-            image = image.resize((36, 36))
-            image_data = asarray(image)
-            tensor = torch.tensor(image_data)
-            print(image_data)
-            print(tensor)
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+
+        return F.softmax(x)
+
+train_data = datasets.MNIST(
+    root='data',
+    train=True,
+    transform=ToTensor(),
+    download=True
+)
+test_data = datasets.MNIST(
+    root='data',
+    train=False,
+    transform=ToTensor(),
+    download=True
+)
 
 
-screen = Screen()
+loaders = {
+    'train': DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1),
+    'test': DataLoader(test_data, batch_size=100, shuffle=True, num_workers=1)
+}
 
+device = torch.device('coda' if torch.cuda.is_available() else 'cpu')
 
-screen.mainloop()
+model = CNN().to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+loss_fn = nn.CrossEntropyLoss()
+
+def train(e):
+    model.train()
+    for i, (data, target) in enumerate(loaders['train']):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        if i % 20 == 0:
+            print(
+                f'train epoch: {e} [{i * len(data)} / {len(loaders["train"].dataset)}] ({100. * i / len(loaders["train"]):.0f}%)]\t{loss.item():.6f}')
+
+def test():
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in loaders['test']:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += loss_fn(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(loaders['test'].dataset)
+    print(f'\ntest set: average loss: {test_loss:.4f}, accuracy {correct} / {len(loaders['test'].dataset)} ({100. * correct / len(loaders["test"].dataset):.0f})%\n')
+
+for e in range(1, 11):
+    train(e)
+    test()
